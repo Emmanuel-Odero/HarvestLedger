123cbe404b012ac57604b5e14c73de9a062264c8
Emmanuel-Odero <emmanuelodero808@gmail.com>
2025-12-06 20:08:40 +0300

    feat: add user journey components index

---
+++ frontend/components/user-journey/index.ts
@@ -0,0 +1,5 @@
+export { RoleSwitcher } from "./RoleSwitcher";
+export { ScenarioSelector } from "./ScenarioSelector";
+export { ScenarioProgress } from "./ScenarioProgress";
+export { NotificationCenter } from "./NotificationCenter";
+export { ActivityFeed } from "./ActivityFeed";
